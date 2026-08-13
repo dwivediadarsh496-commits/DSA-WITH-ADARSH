e5b3b6b74689a5b7432b58be4311a1fead5404e5
Adarsh dwivedi <dwivediadarsh496@gmail.com>
2026-08-14 00:14:15 +0530

    Add files via upload

---
+++ 01_Arrays/881. Boats to Save People.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        people.sort()
+        left = 0
+        right = len(people) - 1
+        boat = 0
+        while left <= right:
+            if people[left] + people[right] <= limit:
+                
+                left +=1
+                
+                
+            
+            right -=1
+
+            boat += 1  
+        return boat
+
+
+        
+        
+        
